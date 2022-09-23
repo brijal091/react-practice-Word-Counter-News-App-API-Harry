20d7674cb3686b6d2ad305a451ace42e684c18d6
Brijal <brijalkansara091@gmail.com>
2022-09-23 14:20:07 +0530

    Revert "Vide 33 Done"
    This reverts commit fb7fe92db810809a6d470c87f57332c78b21daa0.

--- NewsApp/newsapp/src/App.js
+++ NewsApp/newsapp/src/App.js
@@ -13,13 +13,13 @@ export default class App extends Component {
           <Routes>
             {/* Adding key because we are Forcing "News" Component to be Remounted else only url will be changes not the news and othe data, that's why using key we are focing component for remounting */}
             {/* Must Make sure that key should be inside of Component not in Route  */}
-            <Route exact path="/" element = {<News key="1" pageSize = {6} category='general'/>} />
-            <Route exact path="/business" element = {<News  key="2" pageSize = {6} category='business'/>} />
-            <Route exact path="/entertainment" element = {<News key="3" pageSize = {6} category='entertainment'/>} />
-            <Route exact path="/health" element = {<News key="4" pageSize = {6} category='health'/>} />
-            <Route exact path="/science" element = {<News key="5" pageSize = {6} category='science'/>} />
-            <Route exact path="/sports" element = {<News key="6" pageSize = {6} category='sports'/>} />
-            <Route exact path="/technology" element = {<News key="7" pageSize = {6} category='technology'/>} />         
+            <Route exact path="/" element = {<News key="1" pageSize = {5} category='general'/>} />
+            <Route exact path="/business" element = {<News  key="2" pageSize = {5} category='business'/>} />
+            <Route exact path="/entertainment" element = {<News key="3" pageSize = {5} category='entertainment'/>} />
+            <Route exact path="/health" element = {<News key="4" pageSize = {5} category='health'/>} />
+            <Route exact path="/science" element = {<News key="5" pageSize = {5} category='science'/>} />
+            <Route exact path="/sports" element = {<News key="6" pageSize = {5} category='sports'/>} />
+            <Route exact path="/technology" element = {<News key="7" pageSize = {5} category='technology'/>} />         
           </Routes>
         </Router>
       </div>
--- NewsApp/newsapp/src/components/NewsItem.js
+++ NewsApp/newsapp/src/components/NewsItem.js
@@ -3,16 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
     // we need to use props in class based components. 
-    let {title, description, imageUrl, newsUrl, author,date} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
-        <div className="card">
+        <div className="card" style={{width: "18rem"}}>
             <img src={imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
               {/* Using Props  */}
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
-              <p className="card-text"><small className="text-muted">By {author} On {new Date(date).toDateString()}</small></p>
               <a href={newsUrl} arget="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
