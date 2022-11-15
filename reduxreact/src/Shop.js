dd803178add40ab6f153c920b8137d718bfb9985
Brijal <brijalkansara091@gmail.com>
2022-11-15 16:00:46 +0530

    actions and reducers of react

---
+++ reduxreact/src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h1>Deposit/Withdraw money</h1>
+        <button className="btn btn-primary mx-3">--</button>
+        Update Balance
+        <button className="btn btn-primary mx-3">+</button>
+    </div>
+  )
+}
+
+export default Shop
